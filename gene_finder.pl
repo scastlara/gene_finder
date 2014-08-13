4a8a5d4943742a93b965785c42bfc3cdd9a3df48
Sergio Castillo Lara <scastillo@cooper.bio.ub.edu>
2014-08-13 18:14:32 +0200

    Improved readability after multiple 'reverts'

--- gene_finder.pl
+++ gene_finder.pl
@@ -7,8 +7,8 @@
 #						  saves the lines containing them as matches_originalfile.txt
 #
 #			Arguments: 	  Synonyms tabular file
-#						  Text file to analyze
 #						  Stopwords file
+#						  Text file(s) to analyze
 #
 #
 #********************************************************************************
@@ -33,14 +33,13 @@ die "\nYou have to introduce 3 files as command line arguments:\n" .
 my $synonyms 		= shift @ARGV;
 my $stop_words_file = shift @ARGV;
 my @problem_files   = @ARGV;
-my $text 	        = "";
 my %hash_table      = ();
 my %stop_words 		= ();
+
 
 #********************************************************************************
 # MAIN LOOP 
 #********************************************************************************
-
 
 print STDERR "\n## STARTING PROGRAM ##\n## CREATING SYNONYMS HASH TABLE...\n";
 
@@ -85,18 +84,20 @@ sub table_reader {
 
 	# reading webpage line by line
 
-	open (SYN, $synonyms);
+	open (SYN, $synonyms)
+		or die "Can't open $synonyms : $!\n";
+		
 	<SYN>; # skip 1st line
 	
 	while (my $line = <SYN>) {
 		
 		next if ($line =~ m/withdrawn/g);
-		my @fields = split /\t/, $line;
-		my @all_synonyms = ();
+
+		my @fields        = split /\t/, $line;
+		my @all_synonyms  = ();
 		my @normal_fields = ();
 		my @quoted_fields = ();
-		
-		my $Ap_SYMBOL = $fields[0];
+		my $Ap_SYMBOL     = $fields[0];
 		
 		if ($fields[1]) {
 
@@ -108,14 +109,14 @@ sub table_reader {
 		if ($fields[2]) {
 
 			my @Prev_SYMBOL = split /,/, $fields[2];
-			@Prev_SYMBOL = map {$_=~ s/^\s//; $_} @Prev_SYMBOL;
+			@Prev_SYMBOL    = map {$_=~ s/^\s//; $_} @Prev_SYMBOL;
 			push @normal_fields, @Prev_SYMBOL;
 		}; 
 
 		if ($fields[3]) {
 
 			my @SYN = split /,/, $fields[3];
-			@SYN = map {$_=~ s/^\s//; $_} @SYN;
+			@SYN    = map {$_=~ s/^\s//; $_} @SYN;
 			push @normal_fields, @SYN;
 		};
 		
@@ -145,7 +146,7 @@ sub table_reader {
 			while ($prev_NAMES =~ m/"(.+?)"/g) {
 
 				my $match = $1;
-				$match =~ s/^\s//;
+				$match    =~ s/^\s//;
        			push @quoted_fields, $match;
     
    			 }; # while match
@@ -191,14 +192,14 @@ sub table_reader {
 
 sub hash_creator {
 	
-	my $SYMBOL = shift;
+	my $SYMBOL       = shift;
 	my $synonyms_ary = shift;
 
 	# SYMBOL KEYS!
 	
 	if (!exists $hash_table{$SYMBOL}) {
 
-		$hash_table{$SYMBOL} = [];
+		$hash_table{$SYMBOL}      = [];
 		$hash_table{$SYMBOL}->[0] = '0';
 		$hash_table{$SYMBOL}->[1] = undef;
 		$hash_table{$SYMBOL}->[2] = $SYMBOL;
@@ -216,9 +217,9 @@ sub hash_creator {
 
 	foreach my $synonym (@$synonyms_ary) {
 
-		my $ucsynoym = uc $synonym; # make synonyms uppercase
-		$ucsynoym =~ s/[^A-Z0-9\s]//g; # remove non word/number/space characters
-		my @syn_words = split /\s/, $ucsynoym; # get each word of synonym
+		my $ucsynoym   = uc $synonym; # make synonyms uppercase
+		$ucsynoym      =~ s/[^A-Z0-9\s]//g; # remove non word/number/space characters
+		my @syn_words  = split /\s/, $ucsynoym; # get each word of synonym
 		my $first_word = shift @syn_words;
 	
 		return if (!$first_word);
@@ -267,11 +268,11 @@ sub hash_creator {
 
 sub words_ladder {
 	
-	my $SYMBOL = shift;
-	my $syn_words_ary = shift;
+	my $SYMBOL         = shift;
+	my $syn_words_ary  = shift;
 	my $hash_table_hsh = shift;
-	my $synonym = shift;
-	my $next_word = shift @$syn_words_ary;
+	my $synonym        = shift;
+	my $next_word      = shift @$syn_words_ary;
 		
 	if ($hash_table_hsh->[2]) {
 
@@ -283,9 +284,9 @@ sub words_ladder {
 
 	}; # if symbol defined
 	
-	$hash_table_hsh->[1] = {} unless (defined $hash_table_hsh->[1]); # creates hash at position 2 if it's not defined
-	$hash_table_hsh->[2] = undef unless ($hash_table_hsh->[2]); # undefs symbol if it doesn't exist (not true)
-	$hash_table_hsh->[3] = undef unless ($hash_table_hsh->[3]); # undefs original synonym if it doesn't exist (not true)
+	$hash_table_hsh->[1] = {} unless (defined $hash_table_hsh->[1]);
+	$hash_table_hsh->[2] = undef unless ($hash_table_hsh->[2]);
+	$hash_table_hsh->[3] = undef unless ($hash_table_hsh->[3]);
 	$hash_table_hsh->[1]->{$next_word} = [] if (!exists $hash_table_hsh->[1]->{$next_word});
 
 	my $new_hsh = $hash_table_hsh->[1]->{$next_word};
@@ -328,19 +329,24 @@ sub words_ladder {
 
 sub stop_words_reader {
 
-	my $file = shift;
+	my $file           = shift;
 	my $stop_words_hsh = shift;
-	open (WORDS, $file) or die "Can't open stopwords file\n";
+	
+	open (WORDS, $file) 
+		or die "Can't open stopwords file\n";
 	
 	while (<WORDS>) {
 		
 		chomp;
+		
 		next if (/^\s+/g); # skip blank lines
-		my $word_comma = "$_,";
+		
+		my $word_comma    = "$_,";
 		my $word_fullstop = "$_.";
-		$stop_words_hsh->{$_} = undef if (!exists $stop_words_hsh->{$_});
-		$stop_words_hsh->{ucfirst$_} = undef if (!exists $stop_words_hsh->{ucfirst$_} and length$_ > 1);
-		$stop_words_hsh->{$word_comma} = undef if (!exists $stop_words_hsh->{$word_comma});
+
+		$stop_words_hsh->{$_}             = undef if (!exists $stop_words_hsh->{$_});
+		$stop_words_hsh->{ucfirst$_}      = undef if (!exists $stop_words_hsh->{ucfirst$_} and length$_ > 1);
+		$stop_words_hsh->{$word_comma}    = undef if (!exists $stop_words_hsh->{$word_comma});
 		$stop_words_hsh->{$word_fullstop} = undef if (!exists $stop_words_hsh->{$word_fullstop});
 
 	}; # while <WORDS>
@@ -363,20 +369,21 @@ sub stop_words_reader {
 
 sub text_analyzer($$) {
 	
-	my $file = shift;
-	my $hash = shift;
+	my $file           = shift;
+	my $hash           = shift;
 	my $stop_words_hsh = shift;
-	my $outfile = shift;
-	my @tagged_lines = ();
+	my $outfile        = shift;
+	my @tagged_lines   = ();
 
-	open (TEXTFILE, $file);
+	open (TEXTFILE, $file)
+		or die "Can't open $file : $!\n";
 
 	while (my $line = <TEXTFILE>) {
 
 		chomp $line;
-		my @allwords = split /\s/, $line; 
-		my @words = grep { !exists $stop_words_hsh->{$_} } @allwords; # Removes stopwords for processing
-		my @words_copy = @words;
+		my @allwords       = split /\s/, $line; 
+		my @words          = grep { !exists $stop_words_hsh->{$_} } @allwords; # Removes stopwords for processing
+		my @words_copy     = @words;
 		my $positive_lines = "";
 
 		for (my $i = 0; $i < @words;) {
@@ -389,7 +396,8 @@ sub text_analyzer($$) {
 			if ($complete_gene) {
 
 				my @array_count = split /\s/, $complete_gene;
-				my $count = @array_count;
+				my $count       = @array_count;
+				
 				$i += $count; 
 				splice @words_copy, 0, $count;
 				
@@ -404,7 +412,7 @@ sub text_analyzer($$) {
 			
 		} # foreach word
 
-		$positive_lines =~ s/^\s//;
+		$positive_lines  =~ s/^\s//;
 		$positive_lines .= $line if ($line);
 		push @tagged_lines, $positive_lines;
 		
@@ -434,17 +442,17 @@ sub text_analyzer($$) {
 
 sub recurive_search {
 	
-	my $word = shift;
-	my $complete_gene = shift;
-	my $hash = shift;
-	my $line = shift;
+	my $word           = shift;
+	my $complete_gene  = shift;
+	my $hash           = shift;
+	my $line           = shift;
 	my $positive_lines = shift;
-	my @words_copy = @_;
+	my @words_copy     = @_;
 
 	return unless ($word); # ends function if it runs out of words
 
 	my $possible_gene = uc($word);
-	$possible_gene =~ s/[^A-Z0-9\s]//gi;
+	$possible_gene    =~ s/[^A-Z0-9\s]//gi;
 
 	if (exists $hash->{$possible_gene}) {
 
@@ -560,8 +568,10 @@ sub recurive_search {
 sub tagged_lines_filter {
 	
 	my $tagged_lines = shift;
-	my $outfile = shift;
-	open (OUT, "> $outfile");
+	my $outfile      = shift;
+	
+	open (OUT, "> $outfile")
+		or die "Can't open $outfile : $!\n";
 
 	foreach my $line (@$tagged_lines) {
 
