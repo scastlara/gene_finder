0bcebe820fac8cf5cf59ae850ea5d8654aa7ce86
Sergio Castillo <s.cast.lara@gmail.com>
2014-10-15 10:39:54 +0200

    header changed

--- gene_finder.pl
+++ gene_finder.pl
@@ -1,18 +1,37 @@
 #!/usr/bin/perl
 #
-# gene_finder.pl -
-#	This script reads a file that contains a list of gene symbols
-#	and its corresponding synonyms and it creates a synonyms hash table.
-#	Then, it looks for matches in a text file and saves the lines
-#	containing them as matches_originalfile.txt
+#################################################################################
+#                               gene_finder.pl									#
+#################################################################################
+#
+#	This script reads a file that contains a list of gene symbols and its 
+#	corresponding synonyms and it creates a synonyms hash table. Then, it 
+#	looks for matches in a text file and saves the lines containing them as 
+#	matches_originalfile.txt
 #
 #	Arguments:	Synonyms tabular file
 #				Stopwords file
 #				Text file(s) to analyze
 #
 #
-#********************************************************************************
 
+#********************************************************************************
+#        Copyright (C) 2014 - Sergio CASTILLO
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#********************************************************************************
 
 use warnings;
 use strict;
